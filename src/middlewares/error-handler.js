4c870511591738c5324a3765c471a46f41823399
khurshid28 <khurshid_dev@haad.uz>
2024-03-10 02:10:52 +0500

    >

--- src/middlewares/error-handler.js
+++ src/middlewares/error-handler.js
@@ -1,6 +1,9 @@
 module.exports = (error, req, res, next) => {
-	if (error.status < 500) {
-		return res.status(error.status).json(error);
+	if (error.status <= 500 && error) {
+		// return res.status(error.status).json(error);
+		return res.send(({
+			"data":4
+		}))
 	} else {
 		return next(error);
 	}
